ff8586c8bcd9727497cda21561b94faf4844756e
chiraglokhande04 <chiraglokhande14@gmail.com>
2025-01-31 16:55:09 +0530

    commiting backend of day 1

---
+++ routes/user.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const userRouter = express.Router();
+const userController = require("../controllers/user.js");
+
+
+// Public Routes (No Middleware Required)
+userRouter.post('/signup', userController.signup);
+userRouter.post('/login', userController.login);
+userRouter.post('/logout', userController.logout);
+userRouter.post('/verifyEmail', userController.verifyEmail);
+
+
+module.exports = userRouter;
+
