c8b32d7263901517eb2aaf80cbf1ec97413ff6ab
guest3301 <178640023+guest3301@users.noreply.github.com>
2025-01-31 16:58:50 +0530

    Delete helpers directory
    cleaning up the mess.

--- helpers/validation.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-def validate_user_input(data):
-    errors = []
-    if not data.get('name'):
-        errors.append("Name is required")
-    if not data.get('password'):
-        errors.append("Password is required")
-    return errors
